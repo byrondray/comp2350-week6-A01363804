03fa2e8946d17bcbfd69c0fb1efe1de29b1a23db
byron dray <byrondray8@gmail.com>
2024-02-15 20:38:49 -0800

    inital commit

---
+++ week 6 qoddi project files/databaseConnection.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2/promise');
+
+const is_qoddi = process.env.IS_QODDI || false;
+
+const dbConfigQoddi = {
+	host: "sql.freedb.tech",
+	user: "freedb_comp2350_main",
+	password: "XFZSr3p6c25m@qb",
+	database: "dfreedb_comp2350-A01363804",
+	multipleStatements: false
+};
+
+const dbConfigLocal = {
+	host: "localhost",
+	user: "root",
+	password: "Password",
+	database: "database1",
+	multipleStatements: false
+};
+
+if (is_qoddi) {
+	var database = mysql.createPool(dbConfigQoddi);
+}
+else {
+	var database = mysql.createPool(dbConfigLocal);
+}
+
+module.exports = database;
+		
