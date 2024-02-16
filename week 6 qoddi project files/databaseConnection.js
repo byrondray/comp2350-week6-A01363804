207ec143b7e12f959fa4de762f34c4a0b9df13e6
byron dray <byrondray8@gmail.com>
2024-02-15 21:45:23 -0800

    fix json package

--- week 6 qoddi project files/databaseConnection.js
+++ week 6 qoddi project files/databaseConnection.js
@@ -1,32 +1,29 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const is_qoddi = process.env.IS_QODDI || false;
 
 const dbConfigQoddi = {
-	host: "sql.freedb.tech",
-	user: "freedb_comp2350_main",
-	password: "XFZSr3p6c25m@qb",
-	database: "dfreedb_comp2350-A01363804",
-	multipleStatements: false,
-	namedPlaceholders: true
+  host: "sql.freedb.tech",
+  user: "freedb_comp2350_main",
+  password: "XFZSr3p6c25m@qb",
+  database: "freedb_comp2350-A01363804",
+  multipleStatements: false,
+  namedPlaceholders: true,
 };
 
 const dbConfigLocal = {
-	host: "localhost",
-	user: "root",
-	password: "Password",
-	database: "database1",
-	multipleStatements: false,
-	namedPlaceholders: true
-
+  host: "localhost",
+  user: "root",
+  password: "Password",
+  database: "database1",
+  multipleStatements: false,
+  namedPlaceholders: true,
 };
 
 if (is_qoddi) {
-	var database = mysql.createPool(dbConfigQoddi);
-}
-else {
-	var database = mysql.createPool(dbConfigLocal);
+  var database = mysql.createPool(dbConfigQoddi);
+} else {
+  var database = mysql.createPool(dbConfigLocal);
 }
 
 module.exports = database;
-		
